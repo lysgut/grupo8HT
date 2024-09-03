1e0b35966d614a3bd176fbd9f689e98ad7b0f32c
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-03 10:03:00 -0300

    Co-authored-by: tomasriuli <tomasriuli@users.noreply.github.com> Co-authored-by: bianca. <wwwbian@users.noreply.github.com> Co-authored-by: Elmangacadiabolico <Elmangacadiabolico@users.noreply.github.com>

---
+++ hackaton8/src/Components/Conexion.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import React, { useState, useEffect } from 'react';
+function Conexion() {
+    const Url = '3bf624d6860ee0d62f23096a9c412d7a'
+useEffect(()=>{
+axios
+},[])
+  return (
+    <div>Conexion</div>
+  )
+}
+
+export default Conexion
+
+
+
