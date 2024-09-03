d4fbf3ca061979e691811b0fd7a1e1ad5d22230e
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-03 10:03:22 -0300

    h

--- hackaton8/src/Components/Conexion.jsx
+++ hackaton8/src/Components/Conexion.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 function Conexion() {
     const Url = '3bf624d6860ee0d62f23096a9c412d7a'
 useEffect(()=>{
-axios
+axios.get()
 },[])
   return (
     <div>Conexion</div>
